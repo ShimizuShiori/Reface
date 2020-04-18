bfe4c40dc690ff5f5b28cd7b5549c29ed1f17d43
ShimizuShiori <116971263@qq.com>
2020-04-18 11:18:55 +0800

    add some public function

---
+++ src/Reface/Extensions/CollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reface
+{
+
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// 循环并处理每个元素，并返回集合本身
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="handler"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> list, Action<T> handler)
+        {
+            foreach (var item in list)
+                handler(item);
+            return list;
+        }
+
+    }
+}
--- src/Reface/Helpers/EnumHelper.cs
+++ src/Reface/Helpers/EnumHelper.cs
@@ -35,5 +35,29 @@ namespace Reface
             }
             return result;
         }
+
+        public static bool HasFlag<T>(T value, T flag)
+        {
+            if (!typeof(T).IsEnum)
+                return false;
+            int i_value = ConvertToInt32(value);
+            int i_flag = ConvertToInt32(flag);
+            return (i_value & i_flag) == i_flag;
+        }
+
+        public static T RemoveFlag<T>(T value, T flag)
+        {
+            if (!typeof(T).IsEnum)
+                throw new InvalidCastException("T 必须是枚举类型");
+            int i_value = ConvertToInt32(value);
+            int i_flag = ConvertToInt32(flag);
+            int i_result = i_value - (i_value & i_flag);
+            return (T)Enum.ToObject(typeof(T), i_result);
+        }
+
+        private static int ConvertToInt32<T>(T value)
+        {
+            return Convert.ToInt32(value);
+        }
     }
 }
